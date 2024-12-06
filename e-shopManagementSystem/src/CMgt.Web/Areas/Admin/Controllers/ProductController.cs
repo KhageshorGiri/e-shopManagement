a8e686e46100892188801e6a8d391344eedda06a
KhageshorGiri <girikhageshor2432@gmail.com>
2024-12-06 20:37:49 +0545

    Working on product add

--- e-shopManagementSystem/src/CMgt.Web/Areas/Admin/Controllers/ProductController.cs
+++ e-shopManagementSystem/src/CMgt.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,18 +1,31 @@
-﻿using CMgt.shared.ViewModels;
+﻿using CMgt.BLL.IServices;
+using CMgt.shared.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CMgt.Web.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private readonly ICategoryService _categoryService;
+        private readonly ISubCategoryService _subCategoryService;
+        public ProductController(ICategoryService categoryService, ISubCategoryService subCategoryService)
+        {
+            _categoryService = categoryService;
+            _subCategoryService = subCategoryService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            ViewBag.AllCategoriesSelectList = new SelectList(await _categoryService.GetAllCategoriesAsync(), "Id", "CategoryName");
+            ViewBag.AllSubCategoriesSelectList = new SelectList(await _subCategoryService.GetAllSubCategoriesAsync(), "Id", "SubCategoryName");
+
             return View();
         }
 
