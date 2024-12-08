d99c41ca86b04b4285bc88f3a8e02bc0a80bbd12
KhageshorGiri <girikhageshor2432@gmail.com>
2024-12-08 18:50:53 +0545

    Added Order Index Page

---
+++ e-shopManagementSystem/src/CMgt.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMgt.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class OrderController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
