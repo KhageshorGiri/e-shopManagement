43eba97e8c3cafe3506d77a073e1bd6ac267f316
KhageshorGiri <girikhageshor2432@gmail.com>
2025-04-05 11:31:30 +0545

    Workign on CRM module

---
+++ e-shopManagementSystem/src/CMgt.Web/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CMgt.Web.Areas.Admin.Controllers;
+public class CustomerController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
